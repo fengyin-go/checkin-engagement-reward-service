8d436fd941540979ec0cb2549bd568cb93b6f2df
fengyin-go <1621476774@qq.com>
2026-08-19 09:04:58 +0800

    gold: checkin-005

---
+++ pkg/idgen/record_token.go
@@ -0,0 +1,17 @@
+package idgen
+
+import "strings"
+
+func RecordToken(parts ...string) string {
+	cleaned := make([]string, 0, len(parts))
+	for _, part := range parts {
+		part = strings.TrimSpace(part)
+		if part != "" {
+			cleaned = append(cleaned, part)
+		}
+	}
+	if len(cleaned) == 0 {
+		return "record-005"
+	}
+	return strings.Join(cleaned, "-")
+}
